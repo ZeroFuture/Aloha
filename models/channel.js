74a95ce7bfb839942c2eceef1f21e906698ccfa0
Zero Qu <zhidongqu@gmail.com>
2020-04-20 18:23:29 -0700

    remodel backend

--- models/channel.js
+++ models/channel.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
-const group = mongoose.Schema({
+const channel = mongoose.Schema({
     id: {type: String, default: ''},
     name: {type: String, default: ''},
     members: [{
         userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
         username: {type: String, default: ''}
+    }],
+    isGroupChannel: {type: Boolean, default: true},
+    messages: [{
+        message: {type: mongoose.Schema.Types.ObjectId, ref: 'Message' },
     }]
 });
 
-module.exports = mongoose.model('Group', group);
+module.exports = mongoose.model('Channel', channel);
